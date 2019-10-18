866ec9dbdadf7ac33b681258189e26d07406da4b
Haley Glavina <56693294+haleyglavina@users.noreply.github.com>
2019-10-17 23:04:49 -0400

    Add files via upload
    Organizational changes: 
    - foodItem_class.py is its own file and defines basic support functions for handling an instance of a foodItem object
    - functions include: determining how long until expiry, if it's expired, setting new expiry date, and array index of previous copy of that item
    - inputNew.py has intended UI features written as Terminal interactions for the user. Functionalities are there, just not in graphical UI form
    - main.py just has manages which instruction pathway to go down from the GUI's intended home page

--- main.py
+++ main.py
@@ -4,27 +4,41 @@
 # //==============================//
 
 # This program handles UI requests from the HP touch screen. 
-from dataclasses import dataclass
-from PIL import Image
 
-@dataclass
-class foodItem:
-	name: str
-	foodGrp: int
-	inDate: int
-	expDate: int 
-	img: Image
+from foodItem_class import *
+from inputNew import *
+'''
+from inputNew import *
+from inputPreset import *
+from putBack import *
+from retrieve import *
+'''
 
+#==================================================
+# Nonconstants set as constant for testing
 # Requests from memory module if the pantry is full
 def AmIFull():
-	return 1
+	return 0
 
-# Input a New Can Item
-def InputNew(name, foodGroup, expDate, img):
-	# Check if there's room in Pantry
-	if (AmIFull()):
-		print("Cannot fit a new item in the Handy Pantry")
+# Which button on home page is selected
+button = 1
 
-	# Function contents
+#==================================================
 
-	return None
+def main():
+	# Check for user to click on a button
+	if button == 1:
+		if (AmIFull()):
+			print("Your Handy Pantry's storage is full.")
+		else:
+			InputNew()
+			'''
+	elif button == 2: 
+		InputPreset()
+	elif button == 3: 
+		PutBack()
+	elif button == 4: 
+		Retrieve()
+		'''
+main()
+
